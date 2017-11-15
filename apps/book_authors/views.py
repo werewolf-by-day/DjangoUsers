42b1b77213ca8d295ed63f8ebfe8961c66aab2ed
Jason <jason.w.switzer@gmail.com>
2017-11-15 10:59:20 -0800

    removed additional projects from file

--- apps/book_authors/views.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-from django.shortcuts import render, HttpResponse, redirect
-
-def index(request):
-	response = "Hello World"
-	return HttpResponse(response)
--- apps/dojo_ninjas/views.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-def index(request):
-	response = "placeholder"
-	return HttpResponse(response)
